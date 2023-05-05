96c1075da363ff42c6e06e7f1cfeb18f575843f7
Carloslecval <carloslecval03@gmail.com>
2023-05-04 19:34:20 -0600

    contact page responsive and added footer to Home

--- src/Contacto/Contacto.jsx
+++ src/Contacto/Contacto.jsx
@@ -1,41 +1,75 @@
 import React from "react";
 import "./Contacto.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faLocationDot,
+  faEnvelope,
+  faPhone,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Contacto = () => {
-    return (
-        <div className="contacto-container">
-            <div className="titulo-container">
-                <h1>Contacto</h1>
-            </div>
-            <div className="info-container">
-                <h1>Información de contacto</h1>
-            </div>
-            <div className="colaborar-container">
-                <h1>¿Cómo colaborar?</h1>
-                <ul>
-                    <li>Voluntariado</li>
-                    <li>Compártenos en redes sociales</li>
-                    <li>Realiza una donación</li>
-                </ul>
-                <div className="div-boton">
-                    <input class="btn btn-primary" type="button" value="Donar"></input>
-                </div>
-            </div>
-            <div className="formulario-container">
-                <h1>¿Te interesa saber más?</h1>
-                <p>Escríbenos y con gusto nos pondremos en contacto contigo</p>
-                <form className="form-container">
-                    <div className="formRow-container">
-                        <input type="text" placeholder="Nombre" required></input>
-                        <input type="text" placeholder="Correo" required></input>
-                    </div>
-                    <input type="text" placeholder="Asunto" required></input>
-                    <textarea placeholder="Mensaje" required></textarea>
-                    <button>Enviar</button>
-                </form>
-            </div>
+  return (
+    <div className="contacto-container">
+      <div className="titulo-container">
+        <h1>Contacto</h1>
+      </div>
+      <div className="info-container">
+        <div className="contactInfo-container">
+          <FontAwesomeIcon
+            icon={faLocationDot}
+            size="2xl"
+            style={{ color: "#f5802a" }}
+          />
+          <p>Cerrada Salvador Álvarado #7, Col. Escandón, C.P. 11800</p>
         </div>
-    );
+        <div className="contactInfo-container">
+          <FontAwesomeIcon
+            icon={faPhone}
+            size="2xl"
+            style={{ color: "#f5762b" }}
+          />
+          <p>55 1203 6786</p>
+        </div>
+        <div className="contactInfo-container">
+          <FontAwesomeIcon
+            icon={faEnvelope}
+            size="2xl"
+            style={{ color: "#f5762b" }}
+          />
+          <p>uniredst@cemefi.org</p>
+        </div>
+      </div>
+      <div className="colaborar-container">
+        <h1>¿Cómo colaborar?</h1>
+        <ul>
+          <li>Voluntariado</li>
+          <li>Compártenos en redes sociales</li>
+          <li>Realiza una donación</li>
+        </ul>
+        <div className="div-boton">
+          <input
+            className="contacto-button"
+            type="button"
+            value="Donar"
+            id="donar-button"
+          ></input>
+        </div>
+      </div>
+      <div className="formulario-container">
+        <h1>¿Te interesa saber más?</h1>
+        <p>Escríbenos y con gusto nos pondremos en contacto contigo</p>
+        <form className="form-container">
+          <div className="formRow-container">
+            <input type="text" placeholder="Nombre" required></input>
+            <input type="text" placeholder="Correo" required></input>
+          </div>
+          <input type="text" placeholder="Asunto" required></input>
+          <textarea placeholder="Mensaje" required rows={10}></textarea>
+          <button className="contacto-button">Enviar</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Contacto;
--- src/Inicio/Inicio.jsx
+++ src/Inicio/Inicio.jsx
@@ -8,31 +8,34 @@ import MissionLogo from "../Assets/mountain.png";
 import ObjectiveLogo from "../Assets/target.png";
 import VisionLogo from "../Assets/open-eye.png";
 import "./Inicio.css";
-import Footer from "../Layout/Footer";
 
 const Inicio = () => {
   return (
     <>
-      <div className='start-main'>
+      <div className="start-main">
         <img
-          className='start-main-background'
+          className="start-main-background"
           src={Fondo}
-          alt='Fondo principal'
+          alt="Fondo principal"
         />
-        <img className='dark-background img-fluid' src={Darker} alt='Fondo principal' />
-        <div className='start-main-title'>
-          <img className='start-main-logo' src={Logo} alt='Logo de Unired' />
-          <h1 className='start-main-subtitle'>
+        <img
+          className="dark-background img-fluid"
+          src={Darker}
+          alt="Fondo principal"
+        />
+        <div className="start-main-title">
+          <img className="start-main-logo" src={Logo} alt="Logo de Unired" />
+          <h1 className="start-main-subtitle">
             Prevención y Atención de Desastres
           </h1>
         </div>
       </div>
 
-      <div className='white-background'>
-        <div className='who-are-we-container'>
-          <div className='who-are-we-text'>
-            <h1 className='orange-title'>¿Quiénes somos?</h1>
-            <p className='description'>
+      <div className="white-background">
+        <div className="who-are-we-container">
+          <div className="who-are-we-text">
+            <h1 className="orange-title">¿Quiénes somos?</h1>
+            <p className="description">
               La Red Universitaria para la Prevención y Atención de Desastres
               (Unired) es la unión de los principales sistemas universitarios de
               México que trabaja en la prevención y atención de desastres en
@@ -40,40 +43,40 @@ const Inicio = () => {
             </p>
           </div>
           <img
-            className='who-are-we-image'
+            className="who-are-we-image"
             src={WhoAreWe}
-            alt='Quiénes somos'
+            alt="Quiénes somos"
           />
         </div>
       </div>
 
-      <div className='purplish-background'>
-        <div className='mov-container'>
-          <div className='item-container'>
-            <img className='item-logo' src={MissionLogo} alt='Logo misión' />
-            <h1 className='orange-title'>Misión</h1>
-            <p className='item-description'>
+      <div className="purplish-background">
+        <div className="mov-container">
+          <div className="item-container">
+            <img className="item-logo" src={MissionLogo} alt="Logo misión" />
+            <h1 className="orange-title">Misión</h1>
+            <p className="item-description">
               Coordinar acciones universitarias para prevenir y responder ante
               desastres en México
             </p>
           </div>
-          <div className='item-container'>
+          <div className="item-container">
             <img
-              className='item-logo'
+              className="item-logo"
               src={ObjectiveLogo}
-              alt='Logo objetivo'
+              alt="Logo objetivo"
             />
-            <h1 className='orange-title'>Objetivo</h1>
-            <p className='item-description'>
+            <h1 className="orange-title">Objetivo</h1>
+            <p className="item-description">
               Fomentar cultura de prevención y atención ante desastres y
               organizar respuesta adecuada y articulada de la sociedad civil
               ante su ocurrencia.
             </p>
           </div>
-          <div className='item-container'>
-            <img className='item-logo' src={VisionLogo} alt='Logo visión' />
-            <h1 className='orange-title'>Visión</h1>
-            <p className='item-description'>
+          <div className="item-container">
+            <img className="item-logo" src={VisionLogo} alt="Logo visión" />
+            <h1 className="orange-title">Visión</h1>
+            <p className="item-description">
               Ser una red universitaria autosustentable, eficaz y confiable que
               ayude a la sociedad ante desastres.
             </p>
@@ -81,15 +84,15 @@ const Inicio = () => {
         </div>
       </div>
 
-      <div className='antecedent'>
+      <div className="antecedent">
         <img
-          className='antecedent-background'
+          className="antecedent-background"
           src={Antecedent}
-          alt='Antecedentes'
+          alt="Antecedentes"
         />
-        <div className='antecedent-container'>
-          <h1 className='antecedent-title'>Antecedentes</h1>
-          <p className='antecendent-description'>
+        <div className="antecedent-container">
+          <h1 className="antecedent-title">Antecedentes</h1>
+          <p className="antecendent-description">
             Tras el huracán Paulina en 1997, el Cemefi y la Universidad Loyola
             de Acapulco crearon un grupo para ayudar a la población afectada y,
             posteriormente, formaron la red de universidades para promover la
@@ -98,7 +101,6 @@ const Inicio = () => {
           </p>
         </div>
       </div>
-      <Footer />
     </>
   );
 };
